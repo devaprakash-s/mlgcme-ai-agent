0c1fdb81770275a393cdd7a2b4c24e57e91d8647
devaprakash-s <devaprakash.s@gmail.com>
2025-04-29 17:17:28 +0530

    Updated config.py to use environment variables

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
-# config.py
+import os
 
-# MongoDB connection URI
-MONGO_URI = "mongodb+srv://devaprakashs:Omega%21%40%231514@cluster0.rbkoovo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+# MongoDB connection URI from environment variable
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "mlgcme_demo")
 
-
-# MongoDB database name
-DATABASE_NAME = "mlgcme_demo"
-
-# OpenAI API Key (leave blank for now, we will add later)
-OPENAI_API_KEY = "your_openai_api_key_here"
+# Optional: if you use OpenAI later
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
