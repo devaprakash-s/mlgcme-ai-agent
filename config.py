8491c0831bf640c80f44d42f2514e2d4410211c5
devaprakash-s <devaprakash.s@gmail.com>
2025-04-29 17:33:34 +0530

    Updated config.py with new MongoDB password

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
+# config.py
+
+# MongoDB connection URI
+MONGO_URI = "mongodb+srv://devaprakashs:Omega1514@cluster0.rbkoovo.mongodb.net/?retryWrites=true&w=majority"
+
+# MongoDB database name
+DATABASE_NAME = "mlgcme_demo"
+
+# OpenAI API Key (will be loaded from environment on Render)
 import os
-
-# MongoDB connection URI from environment variable
-MONGO_URI = os.environ.get("MONGO_URI")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "mlgcme_demo")
-
-# Optional: if you use OpenAI later
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
